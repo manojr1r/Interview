72d93b5005f7c85a2ca28693951f406e77771139
manoj <manojr1r@gmail.com>
2024-03-21 11:13:15 +0530

    ServiceNow

--- src/main/java/org/example/ServiceNow.java
+++ src/main/java/org/example/ServiceNow.java
@@ -1,22 +1,67 @@
 package org.example;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
-
 public class ServiceNow {
     public static void main(String[] args) {
-        Integer[] no = new Integer[]{0,1 ,0,0,1,1,0,};
-        System.out.println(filterno(no));
-
+        int[] no = new int[]{0, 1, 0, 0, 1, 1, 0};
+        System.out.println(Arrays.toString(onestoRight(no)));
     }
 
-    public static int filterno(Integer[] numbers){
+    public static int[] onestoRight(int[] arr){
+        //approach 1
+
+//        Arrays.sort(arr , Comparator.reverseOrder());
+
+//        //approach 2
+//        int n = arr.length;
+//        for (int i = 0; i < n - 1; i++) {
+//            if (arr[i] == 0 && arr[i + 1] == 1) {
+//                // Swap the elements to shift 1 to the right
+//                int temp = arr[i];
+//                arr[i] = arr[i + 1];
+//                arr[i + 1] = temp;
+//                // Move the index back one step to recheck the swapped element
+//                if (i > 0) {
+//                    i -= 2;
+//                }
+//            }
+//        }
+//
+//        //approach 3
+
+//        int count = 0; // Count of 1s in the array
+//
+//        for (int i : arr) {
+//            if (i == 1) {
+//                count++;
+//            }
+//        }
+//        // Set 1s to the right
+//        for (int i = n - 1; i >= 0; i--) {
+//            if (count > 0) {
+//                arr[i] = 1;
+//                count--;
+//
+//            } else {
+//                arr[i] = 0;
+//            }
+//        }
+//
+        //approach 4
         int one = 0;
-        for (int i = 0; i < (numbers.length ) ; i++) {
-            if(numbers[i] == 1){
+
+        for (int integer : arr) {
+
+            if (integer == 1) {
                 one++;
             }
         }
-        return one;
+
+        arr = new int[arr.length];
+
+        for (int i = (arr.length -1) ; i >= one  ; i--) {
+            arr[i] = 1;
+        }
+        return arr;
     }
 }
--- src/main/java/org/example/ServiceNow2.java
+++ src/main/java/org/example/ServiceNow2.java
@@ -1,20 +1,22 @@
 package org.example;
 
-import com.sun.tools.javac.Main;
-
 public class ServiceNow2 {
     // 72 seats
     //three
-    public enum Berths { LOWER_LEFT ,MIDDLE_LEFT, UPPER_LEFT ,LOWER_RIGHT,MIDDLE_RIGHT, UPPER_RIGHT, LOWER_SIDE, UPPER_SIDE }
+    public enum Berths { UPPER_SIDE, LOWER_LEFT ,MIDDLE_LEFT, UPPER_LEFT ,LOWER_RIGHT,MIDDLE_RIGHT, UPPER_RIGHT, LOWER_SIDE }
 
     public static void main(String[] args) {
-
+        System.out.println(2 +" should be MIDDLE_LEFT = "+ findBerth(2));
+        System.out.println(7 +" should be LOWER_SIDE = " +findBerth(7));
+        System.out.println(8 +" should be UPPER_SIDE = "+  findBerth(8));
+        System.out.println( findBerth(72));
     }
-    public static void findBerth(int seatNo){
-        if(seatNo > 0 && seatNo < 72){
+    public static Berths findBerth(int seatNo){
+        if(seatNo > 0 && seatNo <= 72){
             int no =  seatNo % 8;
-
+            return Berths.values()[no];
         }
+        return null;
          
     }
 }
