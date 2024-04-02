516c68b26513cfaea9e3f5f9d9c31e7d21ef1dc6
manoj <manojr1r@gmail.com>
2024-04-02 13:53:02 +0530

    IBM

---
+++ src/main/java/org/example/IBM.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.io.*;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class IBM {
+        /*
+         * Complete the 'getLatestKRequests' function below.
+         *
+         * The function is expected to return a STRING_ARRAY.
+         * The function accepts following parameters:
+         *  1. STRING_ARRAY requests
+         *  2. INTEGER K
+         */
+
+
+        public static List<String> getLatestKRequests(List<String> requests, int K) {
+            // Write your code here
+            Set<String> single = new LinkedHashSet<>();
+            int count = 0;
+
+            for (int i= (requests.size()-1); i>=0; i--  ) {
+
+                boolean added =  single.add(requests.get(i) );
+                if (added ){
+                    count++;
+                    if(count == K ){
+                        break;
+                    }
+                }
+            }
+            requests.clear();
+
+            requests.addAll(single);
+            return requests;
+        }
+    }
